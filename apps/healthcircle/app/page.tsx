58d95952789882150ed0a4c868809eef138daab0
NishaTwilight <nisha.manibalan@twilightsoftwares.com>
2024-01-30 12:12:05 +0530

    initial setup

---
+++ apps/healthcircle/app/page.tsx
@@ -0,0 +1,23 @@
+import './global.css';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+      <div className="wrapper">
+        <div className="container">
+          <div id="welcome">
+          <h1 className="text-3xl font-bold underline bg-red-500">
+             Hello world!
+            </h1>
+            <h1 className="bg-customText1">
+             Hello world!
+            </h1>
+          </div>
+        </div>
+      </div>
+  );
+}
